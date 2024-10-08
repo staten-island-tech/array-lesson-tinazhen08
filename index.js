e4ff1eafc7b0bfd64fc4eb3b7e3ec40b2a6bdb17
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-10-08 13:56:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,30 @@ const students = [
 
 //Part 1: Practice forEach
 const nameAndAge = students.forEach((student) =>
-  console.log('Name:',student.name,'\nGrade:',student.grade, '\n')
+  console.log("Name:", student.name, "\nGrade:", student.grade, "\n")
 );
 
 //Part 2; Practice with filter
 //1.
-const topStudents = students.filter(student => student.grade > 80);
-console.log('Top Students:',topStudents, "\n");
+const topStudents = students.filter((student) => student.grade > 80);
+console.log("Top Students:", topStudents, "\n");
 
 //2.
-const youngStudents = students.filter(student => student.age <= 21);
-console.log('Young Students',youngStudents, '\n');
+const youngStudents = students.filter((student) => student.age <= 21);
+console.log("Young Students", youngStudents, "\n");
 
 //Part 3: Combine forEach and filter
 //1.
 const above80 = students
-  .filter(student => student.grade > 80)
+  .filter((student) => student.grade > 80)
   .forEach((student) => console.log(student.name));
-console.log(above80, '\n');
+console.log(above80, "\n");
 
 //2.
 const lessThanOrEqualTo21 = students
-  .filter(student => student.age <= 21)
+  .filter((student) => student.age <= 21)
   .forEach((student) => console.log(student.name));
-console.log(lessThanOrEqualTo21, '\n');
+console.log(lessThanOrEqualTo21, "\n");
 
 //Assignment 2: Practicing forEach and filter with Product Data
 //Array of products
@@ -49,12 +49,17 @@ const products = [
 //Part 1: Practice with forEach
 //1.
 const nameAndPrice = products.forEach((product) =>
-  console.log('Name:',product.name,'\nPrice:', product.price, '\n')
+  console.log("Name:", product.name, "\nPrice:", product.price, "\n")
 );
 
 //2.
 const increasedPrice = products.forEach((product) =>
-  console.log('Name:',product.name,'\n Increased Price:',Math.round(product.price * 1.1))
+  console.log(
+    "Name:",
+    product.name,
+    "\n Increased Price:",
+    Math.round(product.price * 1.1)
+  )
 );
 
 //3.
@@ -64,35 +69,33 @@ products.forEach((product) => {
     categories.push(product.category);
   }
 });
-console.log('\n',"Unique Categories:", categories, '\n');
-
-
+console.log("\n", "Unique Categories:", categories, "\n");
 
 //Part 2: Practice with filter
-//1. 
-const electronics = products.filter(product => product.category === 'Electronics');
-console.log('Electronics:', electronics, '\n');
+//1.
+const electronics = products.filter(
+  (product) => product.category === "Electronics"
+);
+console.log("Electronics:", electronics, "\n");
 
-//2. 
-const expensiveProducts = products.filter(product => product.price > 300);
-console.log('>$300 Products:', expensiveProducts, '\n');
+//2.
+const expensiveProducts = products.filter((product) => product.price > 300);
+console.log(">$300 Products:", expensiveProducts, "\n");
 
 //3.
-const highRating = products.filter(product => product.rating >= 4.5);
-console.log('High ratings', highRating, '\n');
-
-
+const highRating = products.filter((product) => product.rating >= 4.5);
+console.log("High ratings", highRating, "\n");
 
 //Part 3: Combine forEach and filter
-//1. 
-console.log('High Rating Product Names:')
+//1.
+console.log("High Rating Product Names:");
 const highRatingNames = products
-  .filter(product => product.rating >= 4.5)
+  .filter((product) => product.rating >= 4.5)
   .forEach((product) => console.log(product.name));
 
-//2. 
-console.log('\n Affordable Electronics:')
+//2.
+console.log("\n Affordable Electronics:");
 const affordableElectronics = products
-  .filter(product => product.category === 'Electronics')
-  .filter(product => product.price < 1000)
+  .filter((product) => product.category === "Electronics")
+  .filter((product) => product.price < 1000)
   .forEach((product) => console.log(product));
